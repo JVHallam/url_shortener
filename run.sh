f07e2ff422c9b262470dbfc38f0e9e016b83821a
jake <jakehallam95@gmail.com>
2021-07-22 22:37:16 +0100

    Removed files with secrets in it

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export connectionString="Server=tcp:jakepayroc.database.windows.net,1433;Initial Catalog=urls;Persist Security Info=False;User ID=jakeadmin;Password=LongPassword1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-
-dotnet watch run;
